2a98492ba9180c7a0a794201298587fd1d9f9423
ihsanfavian831 <ihsanfavian831@gmail.com>
2025-10-24 11:18:49 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,23 @@
+barang1 = "susu"
+harga1 = 15000
+
+barang2 = "roti"
+harga2 = 10000
+
+barang3 = "gula"
+harga3 = 22000
+
+barang4 = "kopi"
+harga4 = 8000
+
+barang5 = "selai"
+harga5 = 17000
+
+total = harga1 + harga2
+print("=== Selamat Datang di Tokomart===")
+print("susu: - Rp %s" %(harga1))
+print("roti: - Rp %s" %(harga2))
+print("gula: - Rp %s" %(harga3))
+print("kopi: - Rp %s" %(harga4))
+print("selai: - Rp %s" %(harga5))
+print("total: susu + roti = Rp%s" %(total))
